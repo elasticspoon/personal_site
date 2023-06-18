cbe57c4f757a951dc1377a14e009fb8e781afd78
Yuri Bocharov <quesadillaman@gmail.com>
2023-06-18 14:06:13 -0400

    wip

--- assets/js/skin-switch.js
+++ assets/js/skin-switch.js
@@ -1,10 +1,27 @@
-$(function() {
-  $('#skin-toggle').on('click', function() {
-    $("body").toggleClass("page-dark-mode");
-    localStorage.setItem('bj-dark-mode', $("body").hasClass("page-dark-mode"));
-    BeautifulJekyllJS.initNavbar();
-  });
-  if (localStorage.getItem('bj-dark-mode') === 'true') {
-    $('#change-skin').trigger('click');
-  }
-});
+// $(function() {
+//   $('#change-skin').on('click', function() {
+//     $("body").toggleClass("page-dark-mode");
+//     $("#change-skin").toggleAttribute('aria-checked');
+//     localStorage.setItem('bj-dark-mode', $("body").hasClass("page-dark-mode"));
+//     BeautifulJekyllJS.initNavbar();
+//   });
+//   if (localStorage.getItem('bj-dark-mode') === 'true') {
+//     $('#change-skin').trigger('click');
+//   }
+// });
+
+const button = document.getElementById('change-skin');
+
+button.addEventListener("click", () => {
+  document.body.classList.toggle('page-dark-mode');
+
+  let currentTheme = document.body.classList.contains('page-dark-mode');
+
+  localStorage.setItem('bj-dark-mode', currentTheme);
+  button.setAttribute('aria-checked', currentTheme);
+  BeautifulJekyllJS.initNavbar();
+
+})
+if (localStorage.getItem('bj-dark-mode') === 'true') {
+  button.click();
+}
