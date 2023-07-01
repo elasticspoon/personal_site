41504fb42dad806b3d5f7e6b06f7eea4fd32ab15
Yuri Bocharov <quesadillaman@gmail.com>
2023-07-01 15:32:04 -0400

    adds postcss preset env

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,8 @@
 // postcss.config.js
 module.exports = {
-  plugins: [require("autoprefixer")],
+  plugins: [
+    require("postcss-preset-env")({
+      enableClientSidePolyfills: true,
+    }),
+  ],
 };
