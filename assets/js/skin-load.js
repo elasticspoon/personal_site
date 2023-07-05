8cd3f9a84d7c0b4e59db0a17c5c31175a6c2b234
Yuri Bocharov <quesadillaman@gmail.com>
2023-07-05 18:52:15 -0400

    swaps skin-swap back to using localstorage

--- assets/js/skin-load.js
+++ assets/js/skin-load.js
@@ -1,23 +1,6 @@
-// Function to get the value of a cookie
-function getCookie(name) {
-  var cookieName = name + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var cookieArray = decodedCookie.split(";");
-
-  for (var i = 0; i < cookieArray.length; i++) {
-    var cookie = cookieArray[i].trim(); // Remove leading/trailing spaces
-
-    if (cookie.substring(0, cookieName.length) === cookieName) {
-      return cookie.substring(cookieName.length, cookie.length);
-    }
-  }
-
-  return null;
-}
-
 window.addEventListener("DOMContentLoaded", () => {
   // (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  var currentTheme = getCookie("bj-dark-mode");
+  var currentTheme = localStorage.getItem("bj-dark-mode");
   var hasDarkSystemTheme = window.matchMedia(
     "(prefers-color-scheme: dark)"
   ).matches;
