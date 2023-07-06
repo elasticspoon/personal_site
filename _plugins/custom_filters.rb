3d0ad1d9241faf48cef53e74e7a6106ed1a1eee6
Yuri Bocharov <quesadillaman@gmail.com>
2023-07-06 19:34:28 -0400

    movie vault wip

--- _plugins/custom_filters.rb
+++ _plugins/custom_filters.rb
@@ -3,6 +3,26 @@ module Jekyll
     def strip_headings(input)
       input.gsub(/<h\d[^>]*>.*?<\/h\d>/i, "")
     end
+
+    def parse_rating(input)
+      half_rating = "½"
+      star = "★"
+
+      case input
+      when Integer
+        half_star = ""
+        rating = input
+      when Float
+        half_star = half_rating
+        rating = input.floor
+      when String
+        return input
+      else
+        raise StandardError, "Invalid input to parse rating. Passed #{input.class} Must be Integer or Float"
+      end
+
+      star * rating + half_star
+    end
   end
 end
 
