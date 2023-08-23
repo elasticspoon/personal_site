c11161db7cb99116279c4384fda95cd2f4a54e71
YuriBocharov <quesadillaman@gmail.com>
2023-08-22 21:10:57 -0400

    wip

--- shell.nix
+++ shell.nix
@@ -48,9 +48,10 @@ stdenv.mkDerivation {
       ln -s "${nodeDependencies}/lib/node_modules" ./node_modules
     fi
     export PATH="${nodeDependencies}/bin:$PATH"
-    alias prod_landing='JEKYLL_ENV="production" bundle exec jekyll serve --config "_config.yml,_config_landing.yml" --trace --livereload'
-    alias prod_blog='JEKYLL_ENV="production" bundle exec jekyll serve --trace --livereload'
-    alias dev_landing='bundle exec jekyll serve --config "_config.yml,_config_landing.yml" --trace --livereload'
-    alias dev_blog='bundle exec jekyll serve --trace --livereload'
+    export NIX_SHELL="true"
+    alias prod_landing='JEKYLL_ENV="production" bundle exec jekyll serve --config "_config.yml,_config_landing.yml" --trace --livereload --livereload-port 35733 --port 3003 -o'
+    alias prod_blog='JEKYLL_ENV="production" bundle exec jekyll serve --trace --livereload --livereload-port 35732 --port 3002 -o'
+    alias dev_landing='bundle exec jekyll serve --config "_config.yml,_config_landing.yml" --trace --livereload --livereload-port 35731 --port 3001 -o'
+    alias dev_blog='bundle exec jekyll serve --trace --livereload --livereload-port 35730 --port 3000 -o'
   '';
 }
