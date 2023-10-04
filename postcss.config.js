db8f599f22628e50324745592deb574f43d1f455
YuriBocharov <quesadillaman@gmail.com>
2023-10-04 14:01:16 -0400

    wip

--- postcss.config.js
+++ postcss.config.js
@@ -5,8 +5,21 @@ module.exports = {
       enableClientSidePolyfills: true,
     }),
     require("@fullhuman/postcss-purgecss")({
-      content: ["_site/**/*.html", "_landing_page/**/*.html"],
-      safelist: ["data-theme"],
+      content: [
+        "_site/**/*.html",
+        "_landing_page/**/*.html",
+        "_site/**/*.js",
+        "_landing_page/**/*.js",
+      ],
+      css: [
+        "_site/assets/css/all.min.css",
+        "_site/assets/css/bootstrap.min.css",
+        "_landing_page/assets/css/all.min.css",
+        "_landing_page/assets/bootstrap.min.css",
+      ],
+    }),
+    require("cssnano")({
+      preset: "default",
     }),
   ],
 };
