1eda33f70ed6fab0eae63afa7b75958d63ba355c
elasticspoon <mail@yuribocharov.dev>
2025-07-13 16:16:01 -0400

    feat: add asset pipeline

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+import './src/beautifuljekyll.js';
+import './src/load-popovers.js';
+import './src/skin-switch.js';
+import * as codapi from '@elasticspoon/codapi/dist/engine/wasi.js';
+import { CodapiSnippet } from '@elasticspoon/codapi/dist/snippet.js';
