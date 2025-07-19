0dd876ce5f1320d7d0269c4990f2140635141918
elasticspoon <mail@yuribocharov.dev>
2025-07-18 20:14:32 -0400

    feat: remove cdn

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,5 @@ import './src/beautifuljekyll.js';
 import './src/load-popovers.js';
 import './src/skin-switch.js';
 import './src/codejar.js';
-import * as codapi from '@elasticspoon/codapi/dist/engine/wasi.js';
-import { Codapisnippet } from '@elasticspoon/codapi/dist/snippet.js';
+import * as codapi from '@elasticspoon/codapi/dist/engine/wasi.mjs';
+import { Codapisnippet } from '@elasticspoon/codapi/dist/snippet.mjs';
