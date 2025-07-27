8f749aa2cf2b8ff7117a645df9b614686afcc539
elasticspoon <mail@yuribocharov.dev>
2025-07-26 22:02:59 -0400

    feat: add ruby version selection in editor

--- assets/js/src/codejar.js
+++ assets/js/src/codejar.js
@@ -20,3 +20,44 @@ document.addEventListener("DOMContentLoaded", () => {
   })
   window.dispatchEvent(new Event("jar-ready"));
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const snippets = document.querySelectorAll("codapi-snippet");
+  snippets.forEach((snippet) => {
+
+    // Create version selector
+    if (snippet.versions && snippet.editor == 'external' && snippet.selector) {
+      const editor = document.querySelector(snippet.selector);
+      const label = document.createElement('label');
+      label.className = 'sr-only';
+      label.textContent = 'Select version';
+      label.htmlFor = `select-${editor.id}`;
+
+      const select = document.createElement('select');
+      select.className = 'playground__version-selector';
+      select.id = label.htmlFor;
+
+      snippet.versions.forEach(version => {
+        const option = document.createElement('option');
+        option.value = version;
+        option.textContent = version;
+        select.appendChild(option);
+      });
+
+      // Set initial version
+      if (snippet.version) {
+        select.value = snippet.version;
+      }
+
+      // Handle version changes
+      select.addEventListener('change', () => {
+        snippet.setVersion(select.value);
+      });
+
+      snippet.style.position = 'relative';
+      const playground = editor.closest('.playground');
+      playground.appendChild(select);
+      playground.appendChild(label);
+    }
+  })
+});
